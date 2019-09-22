37c507f4fe1a10dc17b510befc56622f003bfefd
Corey Lanier <iam@coreylanier.com>
2019-09-21 23:09:51 -0400

    adding event handlers

--- handler.py
+++ handler.py
@@ -7,3 +7,8 @@ streamlabs = socketio.Client()
 streamlabs.connect(f"https://sockets.streamlabs.com?token={socketToken}")
 
 sio.wait()
+
+@sio.event
+def message(eventData)
+    print('something happened')
+    print(eventData)
