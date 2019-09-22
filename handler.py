dc46766b8f30271bb674027c948c2f97f19706f9
Corey Lanier <iam@coreylanier.com>
2019-09-21 23:13:36 -0400

    fixed misnamed connection

--- handler.py
+++ handler.py
@@ -6,9 +6,9 @@ streamlabs = socketio.Client()
 
 streamlabs.connect(f"https://sockets.streamlabs.com?token={socketToken}")
 
-sio.wait()
+streamlabs.wait()
 
-@sio.event
+@streamlabs.event
 def message(eventData)
     print('something happened')
     print(eventData)
